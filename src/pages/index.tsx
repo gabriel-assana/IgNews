1d8f1ddc42ccf2df8a3d97eec45a9791eca335cc
Gabriel Assana <gabrielhhz2@gmail.com>
2023-04-23 19:56:22 -0300

    title dinãnamico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import style from '../styles/home.module.scss';
+import Head from 'next/head';
 
+import style from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
+      <Head>
+          <title>Inicio | ig.news</title>
+      </Head>
       <h1 className={style.title}>
+        
         Hello World
       </h1>
     </>
