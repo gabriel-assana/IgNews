2d5c3d2e30ee0fb5aeaaa83f850c40c1a71f2416
Gabriel Assana <gabrielhhz2@gmail.com>
2023-04-23 21:17:55 -0300

    Componente: Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import Head from 'next/head';
 
-import style from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
           <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={style.title}>
-        
+      <h1>
         Hello World
       </h1>
     </>
