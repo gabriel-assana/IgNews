2f5ea0bd01ea8a7c7b587bb90c2f14e1e9614426
Gabriel Assana <gabrielhhz2@gmail.com>
2023-07-05 00:43:55 -0300

    API routes no Next.js

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,14 +1,15 @@
 import Stripe from 'stripe';
 
-import { version } from '../../package.json';
+import packageInfo from '../../package.json';
 
+//import { version } from '../../package.json';
 export const stripe = new Stripe(
     process.env.STRIPE_API_KEY,
     {
         apiVersion: '2022-11-15',
         appInfo:{
             name: 'Ignews',
-            version
+            version: packageInfo.version
         },
     }
 )
