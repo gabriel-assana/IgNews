92764a6ad577b2f08d8cf0dd85e16fe916d94a31
gabriel-assana <gabrielhhz2@gmail.com>
2024-01-29 21:38:34 -0300

    Alterações

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,50 +1,47 @@
-import { useSession, signIn } from 'next-auth/client';
-import { api } from '../../services/api';
-import styles from './styles.module.scss';
-import { getStripeJs } from '../../services/stripe-js';
-import { Any } from 'faunadb';
+import { useSession, signIn } from 'next-auth/client'
 import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss'
 
+export function SubscribeButton () {
+  const [session] = useSession()
+  const router = useRouter()
 
-interface SubscribeButtoProps {
-    priceId: string;
-}
-
-export function SubscribeButton({ priceId }:SubscribeButtoProps){
-    const [session] = useSession();
-    const router = useRouter()
-
-    async function handleSubscirbe() {
-        if (!session){
-            signIn('github')
-            return;
-        }
-
-        if (session.activeSubscription) {
-            router.push('/posts')
-            return;
-        }
-
-        try {
-            const response = await api.post('/subscribe')
-
-            const { sessionId } =  response.data
-
-            const stripe = await getStripeJs()
-
-            await stripe.redirectToCheckout({ sessionId })
-        } catch (err){
-            alert(err.message);
-        }        
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
     }
 
-    return(
-        <button
-            type="button"
-            className={styles.subscribeButton}
-            onClick={handleSubscirbe}
-        >
-            Subscribe now
-        </button>
-    );
+    if (session.activeSubscription) {
+      router.push('/posts')
+
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      stripe.redirectToCheckout({
+        sessionId
+      })
+    } catch(err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button 
+      type="button"
+      className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
+    >
+      Subscribe now
+    </button>
+  )
 }
